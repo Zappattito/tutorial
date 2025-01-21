1256705e1cd17de7f0995d6d7b7b3b627f53c6be
Zappattito <algarve318@gmail.com>
2025-01-21 15:12:10 +0100

    formulario

--- src/app/components/details/details.component.ts
+++ src/app/components/details/details.component.ts
@@ -1,12 +1,14 @@
 import { Component, inject } from '@angular/core';
+import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
 import { HousingService } from '../housing.service';
 import { HousingLocation } from '../housing-location';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
+import { last } from 'rxjs';
 
 @Component({
   selector: 'app-details',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './details.component.html',
   styleUrl: './details.component.css'
 })
@@ -15,9 +17,25 @@ export class DetailsComponent {
   housingService = inject(HousingService);
   housingLocation: HousingLocation | undefined;
 
+  formAngular = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    email: new FormControl(''),
+
+})
+
   constructor() {
     const housingLocationId = Number(this.route.snapshot.params['id']);
     this.housingLocation = this.housingService.getHousingLocationById(housingLocationId)
+  }
+
+  onSubmit() {
+    this.housingService.submitApplication(
+      this.formAngular.value.firstName ?? '',
+      this.formAngular.value.lastName ?? '',
+      this.formAngular.value.email ??''
+    )
+
   }
 
 }
